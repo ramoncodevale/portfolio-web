07cb6343bbf45a760b3b6c546f788de6c52b0e32
ramoncodevale <ramonvalentim88@gmail.com>
2023-12-15 18:09:25 -0300

    'link'

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -10,17 +10,21 @@ const Inicio = () => {
     <>
       <div className='inicio'>
         <div className="inicio-container">
-        <h1 className='titulo-inicio'>Eu sou Ramon Valentim <br></br><span>Desenvolvedor Front-End <br></br></span><br></br>
-        </h1>
+          <h1 className='titulo-inicio'>Eu sou Ramon Valentim <br></br><span>Desenvolvedor Front-End <br></br></span><br></br>
+          </h1>
           <p className='texto-inicio'>Eu crio sites responsivos onde a tecnologia encontra <br></br> a criatividade</p>
-      <div className="botao-container">
-        <button>Linkedin <FaLinkedin  fontSize={16}/></button>
-        <button>Github <FaGithub fontSize={16} /></button>
-      </div>
+          <div className="botao-container">
+            <a href="https://www.linkedin.com/in/ramon-valentim-3435541a8/" target='__blank'>
+              <button>Linkedin <FaLinkedin fontSize={16} /></button>
+            </a>
+            <a href="https://github.com/ramoncodevale" target='__blank'>
+              <button>Github <FaGithub fontSize={16} /></button>
+            </a>
+          </div>
         </div>
-       <div className='img-perfil'>
-        <img className='foto-perfil' src={Foto} alt='foto de perfil' />
-       </div>
+        <div className='img-perfil'>
+          <img className='foto-perfil' src={Foto} alt='foto de perfil' />
+        </div>
       </div>
     </>
   )
